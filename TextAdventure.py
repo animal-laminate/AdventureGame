b1caaddf9fd468e4cc1a0bb641ab5e935b7b0070
John Halloran <aa1910@coventry.ac.uk>
2018-10-17 18:32:26 +0000

    Commit 2

--- TextAdventure.py
+++ TextAdventure.py
@@ -2,6 +2,7 @@ print("----------4000CEM Week 4 Lab 2 Task----------")
 print("--------------Bored Of The Dings-------------")
 print("-----------------<G and Alf>-----------------")
 print("\n")
+print("THIS IS CHANGE 19.31 17.10.18")
 
 print("You awake in a dark room.  Do you:")
 print("a) Scream for help.")
@@ -14,9 +15,13 @@ if x == "a":
     print("b) Do nothing else")
     x = input("Enter a or b: ")
     if x == "a":
-        print("At a nested if")
+        print("At a nested if")       
     elif x == "b":
         print ("At another nested if")
+        print("Do you:")
+        print("a) Exclaim loudly that you cannot possibly keep track of where you are in this game.")
+        print("b) Say No problem I know exactly where I am")
+        x = input("Enter a or b: ")
 elif x == "b":
     print("The light comes on.")
     print("You do not recognise the room but you have a really bad feeling...")
